6c62b648eca09040e1ef6a9279927de51381f94d
Emre Anıl Terzioğlu <emreanilterzioglu@gmail.com>
2022-08-08 22:26:07 +0300

    [Daily Commit] Sentence extraction from text mechanism added, everything is working fine.

--- freqCounter.py
+++ freqCounter.py
@@ -1,5 +1,5 @@
 import os.path
-
+import nltk
 
 def updateWordFreqData(sentence, freqDict, sentenceNo):
     for word in sentence.split():
@@ -10,10 +10,20 @@ def updateWordFreqData(sentence, freqDict, sentenceNo):
          
 def main():
     freqDict = {}
-
+    
+    # list of specialWordsList. List can be updated
+    specialWordsList = ["Mr.", "St.", "Mrs.", "Ms.", "Dr.", 
+    "Inc.", "Ltd.", "Jr.", "Sr.", "Co.", 
+    ".com", ".net", ".org", ".io", ".gov", ".edu",
+    "i.e.", "etc.", "e.g."]
+    
     # list of unwanted delimeters. List can be updated
     unwantedDelimeters = [';', ',', ':']
-    
+
+    # Natural Language ToolKit Init
+    #nltk.download('punkt')
+    tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+   
     # Check for file existance
     if not os.path.exists("./textFile.txt"):
         print("[ERROR]: textFile.txt does not exist!")
@@ -21,20 +31,51 @@ def main():
     
     # Read and Check for the string size
     textFile = open("./textFile.txt", "r")
-    sentence = textFile.read()
-    if len(sentence) == 0:
+    text = textFile.read()
+    if len(text) == 0:
         print("[ERROR]: textFile.txt is empty!")
         return
     
-    # Remove sentence ending delimeters. -1 is string termination -2 should be delimeter
-    if (sentence[-2] == ".") or (sentence[-2] == "!") or (sentence[-2] == "?"):
-        sentence = sentence[:-2]
     
-    # Remove unwanted delimeters
-    for delimeter in unwantedDelimeters:
-        sentence = sentence.replace(delimeter, '')
+    # Split text into sentences
+    sentences = []
+    faultyDividedSentenceFlag = False
+    for tmpSentence in tokenizer.tokenize(text): 
+        tmpWordList = tmpSentence.split() 
+
+        if faultyDividedSentenceFlag == True:
+            sentences[-1] = sentences[-1] + " " + tmpSentence
+            faultyDividedSentenceFlag = False 
+            for specialWord in specialWordsList:
+                if (specialWord in tmpWordList[-1]):
+                    faultyDividedSentenceFlag = True   
+        else:
+            for specialWord in specialWordsList:
+                if (specialWord in tmpWordList[-1]):
+                    faultyDividedSentenceFlag = True   
+            sentences.append(tmpSentence)
+
+                
+    
+    # Edit delimeters inside sentences then process that sentences
+    sentenceCount = 0
+    for sentence in sentences: 
+        sentenceCount +=1    
+        # Remove sentence ending delimeters. -1 is string termination -2 should be delimeter
+        if (sentence[-1] == ".") or (sentence[-1] == "!") or (sentence[-1] == "?"):
+            sentence = sentence[:-1]
         
-    updateWordFreqData(sentence, freqDict, 1)
+        # Remove unwanted delimeters
+        for delimeter in unwantedDelimeters:
+            delimeterPoses = ( [pos for pos, char in enumerate(sentence) if char == delimeter])
+            if len(delimeterPoses) == 0:
+                continue
+                
+            for pos in delimeterPoses:
+                    if(sentence[pos + 1] == " "):
+                        sentence = sentence[:pos] + " " + sentence[pos + 1:]
+            
+        updateWordFreqData(sentence, freqDict, sentenceCount)
     
     
     # Print Pretty Output 
